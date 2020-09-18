a362760645b5ff78f246735784a98f0810ba0574
atom-young <atom.yang@aelf.io>
2020-09-19 01:14:44 +0800

    feat: 🎸 minus pool balance and update bonus per block

--- src/utils/getEthChainInfo.ts
+++ src/utils/getEthChainInfo.ts
@@ -1,24 +1,27 @@
 const CHAIN_ENV: string = process.env.REACT_APP_CHAIN_ENV || 'main';
 
 interface CHAIN_INFO {
-    chainId: number,
-    rpcUrl: string,
-    ethscanType: string
+  chainId: number,
+  rpcUrl: string,
+  ethscanType: string,
+  stakingPool: string
 }
 
 const CHAIN_ENV_MAP: any = {
-    main: {
-        chainId: 1,
-        rpcUrl: 'https://mainnet.eth.aragon.network/',
-        ethscanType: ''
-    },
-    kovan: {
-        chainId: 42,
-        rpcUrl: 'https://kovan.infura.io/',
-        ethscanType: 'kovan.'
-    }
+  main: {
+    chainId: 1,
+    rpcUrl: 'https://mainnet.eth.aragon.network/',
+    ethscanType: '',
+    stakingPool: '0x1DaeD74ed1dD7C9Dabbe51361ac90A69d851234D'
+  },
+  kovan: {
+    chainId: 42,
+    rpcUrl: 'https://kovan.infura.io/',
+    ethscanType: 'kovan.',
+    stakingPool: '0x1DaeD74ed1dD7C9Dabbe51361ac90A69d851234D'
+  }
 };
 
 export function getEthChainInfo(): CHAIN_INFO {
-    return CHAIN_ENV_MAP[CHAIN_ENV];
-};
+  return CHAIN_ENV_MAP[CHAIN_ENV];
+}
